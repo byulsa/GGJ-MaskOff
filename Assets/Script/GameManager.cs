6e00a88af70bbead09484d38b49f61948a5a9b02
byulsa <a01086269343@gmail.com>
2026-01-31 00:14:11 +0900

    GGJ 1

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gameManager;
+    public bool IsSelect = false;
+    public GameObject SelectCard;
+    void Awake()
+    {
+        gameManager = this;
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void SelectingCard(GameObject Obejct)
+    {
+        if (!IsSelect)
+        {
+            IsSelect = true;
+            SelectCard = Obejct;
+        }
+    }
+    public void PlaceCard()
+    {
+        if (IsSelect)
+        {
+            IsSelect = false;
+        }
+    }
+}
